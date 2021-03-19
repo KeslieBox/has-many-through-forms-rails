6cd118d4b2f9a0cf1a05fadef37487964208f09e
KeslieBox <kesliebox@gmail.com>
2021-03-18 21:58:13 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ActiveRecord::Base
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.post_categories.build(category: category)
+      #or
+      #self.categories << category
     end
   end
 
